93c8bf0ca80ea78a557dfab98fdfb1f9ec267c54
sivaramreddyy <88658247+sivaramreddyy@users.noreply.github.com>
2021-08-31 12:13:08 +0530

    Update DockerJenkinsIntegrationExampleApplication.java

--- src/main/java/com/example/demo/DockerJenkinsIntegrationExampleApplication.java
+++ src/main/java/com/example/demo/DockerJenkinsIntegrationExampleApplication.java
@@ -11,7 +11,7 @@ public class DockerJenkinsIntegrationExampleApplication {
 
 	@GetMapping("/portal")
 	public String getMessage() {
-		return "Spring Jenkin Docker Integration  example!!!";
+		return "Spring Jenkin Docker Integration  example test branch!!!";
 		
 	}
 	public static void main(String[] args) {
